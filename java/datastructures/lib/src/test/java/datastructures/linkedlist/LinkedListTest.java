a1e4742c4476b0a7afc72b05d06e623e3329598d
Jennifer Sung <sung.jenni93@gmail.com>
2023-07-15 22:55:50 -0400

    add unit test and tasks

--- java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
+++ java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
@@ -1,15 +1,30 @@
 package datastructures.linkedlist;
 
+
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class LinkedListTest
 {
-  @Test void testAMethod()
-    {
-      LinkedList sut = new LinkedList();
-      System.out.println(sut);
-      assertTrue(true);
-    }
+
+  @Test public void emptyLinkedList() {
+    LinkedList linkedList = new LinkedList();
+    assertEquals("null", linkedList.toString());
+  }
+
+  @Test void insertLinkedList() {
+    LinkedList linkedList = new LinkedList();
+    linkedList.insert(5);
+    linkedList.insert(10);
+    assertEquals("{ 10 } -> { 5 } -> null", linkedList.toString());
+  }
+
+
+
+
 }
